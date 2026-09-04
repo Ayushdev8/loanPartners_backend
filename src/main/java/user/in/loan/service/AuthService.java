9e61ca6332e1c289fc8e8a62d35bb0253ab19d49
Ayush <akjayushkumarjha8@gmail.com>
2026-09-04 20:30:20 +0530

    authservice

--- src/main/java/user/in/loan/service/AuthService.java
+++ src/main/java/user/in/loan/service/AuthService.java
@@ -24,7 +24,7 @@ public class AuthService {
 
         // 1. Check username
         if (partnerAdminRepository.existsByEmail(request.getPartnerEmail())) {
-            throw new RuntimeException("user already exists");
+            throw new RuntimeException("A user with this email already exists");
         }
         Partner partner = partnerRepository.findByNameAndPartnerTypeAndDistrict(
                 request.getPartnerName(),request.getPartnerType(),request.getDistrict()
@@ -48,7 +48,38 @@ public class AuthService {
                 .createdAt(LocalDateTime.now())
                 .build();
 
+        String state = request.getState();
 
+        if (state != null) {
+
+            state = state.trim().toLowerCase();
+
+            if (state.equals("bihar")) {
+
+                newPartner.setLatitude(25.58931160506634);
+                newPartner.setLongitude(85.1157013495663);
+
+            } else if (state.equals("uttarpradesh") ||
+                    state.equals("up")|| state.equals("uttar pradesh")) {
+
+                newPartner.setLatitude(26.850434383091088);
+                newPartner.setLongitude(80.90610152983272);
+
+            } else if (state.equals("rajasthan")) {
+
+                newPartner.setLatitude(26.968254636556335);
+                newPartner.setLongitude(75.96878790146295);
+
+            } else if (state.equals("gujarat")) {
+
+                newPartner.setLatitude(23.01536887971253);
+                newPartner.setLongitude(72.63289417309376);
+            } else if (state.equals("delhi")){
+                newPartner.setLatitude(28.600319070114047);
+                newPartner.setLongitude(77.20722191200949);
+
+            }
+        }
         partnerRepository.save(newPartner);
 
         // 3. Create PartnerAdmin
